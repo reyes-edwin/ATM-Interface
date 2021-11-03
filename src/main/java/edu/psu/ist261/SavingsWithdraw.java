2df271171f596a577931e4c127f70966ea7c881b
Edwin Reyes <reyesedwin2022@gmail.com>
2021-11-03 10:44:44 -0400

    Adding Panels

--- src/main/java/edu/psu/ist261/SavingsWithdraw.java
+++ src/main/java/edu/psu/ist261/SavingsWithdraw.java
@@ -8,14 +8,19 @@ import java.util.Random;
 
 public class SavingsWithdraw extends SavingsAccount {
     JFrame aframe = new JFrame();
+    JPanel apanel = new JPanel();
 
     public SavingsWithdraw() {
         super();
         aframe.setSize(700, 530);
         aframe.setTitle("Balance");
         aframe.getContentPane().setBackground(Color.decode("#7199BD"));
-
         aframe.setLayout(null);
+
+        apanel.setSize(637, 300);
+        apanel.setLayout(null);
+        apanel.setBackground(Color.decode("#7199BD"));
+
 
         JLabel title = new JLabel("ReyesATM");
         title.setBounds(540, 50, 132, 28);
@@ -35,6 +40,7 @@ public class SavingsWithdraw extends SavingsAccount {
             }
         });
         aframe.add(backArrow);
+
         JButton back = new JButton("Home");
         back.setBounds(55, 53, 80, 30);
         back.setFont(new Font("Helvetica", Font.BOLD, 20));
@@ -53,7 +59,7 @@ public class SavingsWithdraw extends SavingsAccount {
         type.setBounds(55, 131, 180, 23);
         type.setFont(new Font("Helvetica", Font.BOLD, 20));
         type.setForeground(Color.white);
-        aframe.add(type);
+        apanel.add(type);
 
         Random random = new Random();
         String accountNum = String.format("%09d", random.nextInt(1000000000));
@@ -62,59 +68,70 @@ public class SavingsWithdraw extends SavingsAccount {
         acctNum.setBounds(55, 162, 100, 23);
         acctNum.setFont(new Font("Helvetica", Font.PLAIN, 20));
         acctNum.setForeground(Color.white);
-        aframe.add(acctNum);
+        apanel.add(acctNum);
 
         JLabel bal = new JLabel("$" + getBalance());
         bal.setBounds(536, 131, 180, 23);
         bal.setFont(new Font("Helvetica", Font.PLAIN, 20));
         bal.setForeground(Color.white);
-        aframe.add(bal);
+        apanel.add(bal);
 
         JLabel description = new JLabel("Balance");
         description.setBounds(548, 162, 75, 23);
         description.setFont(new Font("Helvetica", Font.PLAIN, 20));
         description.setForeground(Color.white);
-        aframe.add(description);
+        apanel.add(description);
 
         JButton twenty = new JButton("$20");
         twenty.setBounds(55, 235, 150, 50);
         twenty.setFont(new Font("Helvetica", Font.PLAIN, 18));
         twenty.setBackground(Color.white);
-        aframe.add(twenty);
+        twenty.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        apanel.add(twenty);
 
         JButton forty = new JButton("$40");
         forty.setBounds(270, 235, 150, 50);
         forty.setFont(new Font("Helvetica", Font.PLAIN, 18));
         forty.setBackground(Color.white);
-        aframe.add(forty);
+        apanel.add(forty);
 
         JButton sixty = new JButton("$60");
         sixty.setBounds(470, 235, 150, 50);
         sixty.setFont(new Font("Helvetica", Font.PLAIN, 18));
         sixty.setBackground(Color.white);
-        aframe.add(sixty);
+        apanel.add(sixty);
 
         JButton eighty = new JButton("$80");
         eighty.setBounds(55, 319, 150, 50);
         eighty.setFont(new Font("Helvetica", Font.PLAIN, 18));
         eighty.setBackground(Color.white);
-        aframe.add(eighty);
+        apanel.add(eighty);
 
         JButton oneHundred = new JButton("$100");
         oneHundred.setBounds(270, 319, 150, 50);
         oneHundred.setFont(new Font("Helvetica", Font.PLAIN, 18));
         oneHundred.setBackground(Color.white);
-        aframe.add(oneHundred);
+        apanel.add(oneHundred);
 
         JButton other = new JButton("other");
         other.setBounds(470, 319, 150, 50);
         other.setFont(new Font("Helvetica", Font.PLAIN, 18));
         other.setBackground(Color.white);
-        aframe.add(other);
+        apanel.add(other);
 
 
         aframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         aframe.setResizable(false);
         aframe.setVisible(true);
+        apanel.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SavingsWithdraw();
     }
 }
